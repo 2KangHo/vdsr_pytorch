54ece1a379fae68d2e1781377f77ac40a3f46821
ho7719 <ho7719@gmail.com>
2019-01-24 14:29:05 +0900

    Update model.py

--- model.py
+++ model.py
@@ -19,9 +19,9 @@ class VDSR(nn.Module):
         super(VDSR, self).__init__()
         self.residual_layer = self.make_layer(Conv_ReLU_Block, 18)
         self.input = nn.Conv2d(
-            in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+            in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.output = nn.Conv2d(
-            in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1, bias=False)
+            in_channels=64, out_channels=3, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
 
         for m in self.modules():
